b5d40ac709dfacdc622caf3f9fc46af996aca4ae
Carmen Ger <gercanepacarmen@gmail.com>
2025-07-04 21:32:20 -0400

    corregir ruta importacion css

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/index.css";
+import "../styles/index.css";
 import Home from "./components/Home";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
