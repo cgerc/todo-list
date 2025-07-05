e47506375df0e6a690b500fb5d160dbe26feb1f7
Carmen Ger <gercanepacarmen@gmail.com>
2025-07-04 21:05:54 -0400

    agregando estilo

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-//Bootstrap
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "./styles/index.css";
+import Home from "./components/Home";
 
-// index.css'
-import '../styles/index.css'
 
-// components
-import Home from './components/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
